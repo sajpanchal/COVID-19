4756e7dd700c2d83f648ae8beab3ea06b7b17932
Saj Panchal <sajpanchal@gmail.com>
2020-04-16 22:30:13 -0400

    Initial Commit

---
+++ COVID-19/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  COVID-19
+//
+//  Created by saj panchal on 2020-04-16.
+//  Copyright © 2020 saj panchal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ COVID-19Tests/COVID_19Tests.swift
@@ -0,0 +1,34 @@
+//
+//  COVID_19Tests.swift
+//  COVID-19Tests
+//
+//  Created by saj panchal on 2020-04-16.
+//  Copyright © 2020 saj panchal. All rights reserved.
+//
+
+import XCTest
+@testable import COVID_19
+
+class COVID_19Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
